cdc1811e9b000439865dfa0274b93f23e32fb9a6
vamsikrishna71 <vamsicse0@gmail.com>
2022-09-18 12:08:46 +0530

    first commit

---
+++ database/migrations/2022_09_18_062750_create_image_manipulations_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('image_manipulations', function (Blueprint $table) {
+            $table->id();
+            $table->string('name',255);
+            $table->string('path',2000);
+            $table->string('type',255);
+            $table->text('data');
+            $table->timestamp('created_at');
+            $table->string('output_length',2000);
+            $table->foreignIdFor(App\Models\Album::class,'album_id');
+            $table->foreignIdFor(App\Models\User::class,'user_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('image_manipulations');
+    }
+};
